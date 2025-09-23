ed888b612c5c3f09733fef1c73a8567a3f4e674e
Maicom Ferreira <maicomferre3242@gmail.com>
2025-09-23 13:30:55 -0300

    Update nivel Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 struct Carta {
     char estado;
     char cod_carta[10];
-    unsigned int populacao;
-    char nome[60]; //considerando a cidade Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch(59)
+    unsigned long int populacao;
+    char nome[60]; //considerando a cidade Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch(59) + '\0'
     double area;
     double PIB;
     int pontos_turisticos;
+    double super_poder;
+    double pib_per_capta;
+    double densidade_populacional;
 };
 
 void obter_carta(struct Carta *c);
-void exibir_cartas(struct Carta *carta1, struct Carta *carta2);
+void exibir_carta(struct Carta *carta);
+void exibir_vencedor(const struct Carta *carta1, const struct Carta *carta2);
 
 int main(void) {
     struct Carta carta1;
@@ -21,26 +26,34 @@ int main(void) {
     printf("~~~~~~~~~~~~~~ ||  Super Trunfo || ~~~~~~~~~~~~~~\n");
 
     sleep(1);
-    printf("\t\t\tBem vindo ao Jogo.\n");
+    printf("\t\t\t\tBem vindo ao Jogo.\n");
     sleep(1);
     printf("Para começar cadastre 2 cartas\n");
     sleep(2);
     printf("Começe com a primeira:\n");
-    sleep(3);
+    sleep(2);
 
     obter_carta(&carta1);
 
     sleep(1);
     printf("\nPerfeito! Agora preencha a segunda carta.\n");
     printf(">----------------- | 2ª CARTA | >>>>\n\n");
-    sleep(3);
+    sleep(1);
 
+    //Faz a coleta do stdin para a carta
     obter_carta(&carta2);
 
     sleep(1);
 
     printf("Comparação entre as cartas: ");
-    exibir_cartas(&carta1,&carta2);
+
+    printf("\n\nCarta 1:\n\n");
+    exibir_carta(&carta1);
+    printf("\n\nCarta 2:\n\n");
+    exibir_carta(&carta2);
+
+    sleep(5);
+    exibir_vencedor(&carta1, &carta2);
     return 0;
 }
 
@@ -53,10 +66,10 @@ void obter_carta(struct Carta *c) {
     scanf(" %s", c->cod_carta);
 
     printf("\no nome da cidade(Ex: São Paulo): ");
-    scanf(" %60[^\n]", c->nome);
+    scanf(" %59[^\n]", c->nome);
 
     printf("\na população da cidade: ");
-    scanf("%u", &c->populacao);
+    scanf("%lu", &c->populacao);
 
     printf("\na área da cidade(Ex: 17.5): ");
 
@@ -67,37 +80,62 @@ void obter_carta(struct Carta *c) {
 
     printf("\nOs pontos turisticos(Ex: 12): ");
     scanf(" %i", &c->pontos_turisticos);
+
+
 }
 
 
-void exibir_cartas(struct Carta *carta1, struct Carta *carta2) {
-    // Carta 1
-    double densidade_populacao = carta1->populacao / carta1->area;
-    double PIB_per_capta = (carta1->PIB * 1e9) / (double)carta1->populacao;
+void exibir_carta(struct Carta *carta) {
+    carta->densidade_populacional = (double)carta->populacao / carta->area;
+    carta->pib_per_capta = (carta->PIB * 1e9) / (double)carta->populacao;
 
-    printf("\nCarta 1:\n\n");
-    printf("Estado: %c\n", carta1->estado);
-    printf("Código: %s\n", carta1->cod_carta);
-    printf("Nome da Cidade: %s\n", carta1->nome);
-    printf("População: %u\n", carta1->populacao);
-    printf("Área: %.2f km²\n", carta1->area);
-    printf("PIB: %.2f bilhões de reais\n", carta1->PIB);
-    printf("Número de Pontos Turísticos: %d\n", carta1->pontos_turisticos);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacao);
-    printf("PIB per Capita: %.2f reais\n", PIB_per_capta);
+    carta->super_poder = (double)carta->populacao +
+             carta->area +
+             carta->PIB +
+             (double)carta->pontos_turisticos +
+             carta->pib_per_capta +
+             (1.0 / carta->densidade_populacional);
 
-    // Carta 2
-    densidade_populacao = carta2->populacao / (double)carta2->area;
-    PIB_per_capta = (carta2->PIB * 1e9) / (double)carta2->populacao;
+    printf("%-30s %c\n", "Estado:", carta->estado);
+    printf("%-30s %s\n", "Código:", carta->cod_carta);
+    printf("%-30s %s\n", "Nome da Cidade:", carta->nome);
+    printf("%-30s %lu\n", "População:", carta->populacao);
+    printf("%-30s %.2f km²\n","Área:", carta->area);
+    printf("%-30s %.2f bilhões de reais\n","PIB:", carta->PIB);
+    printf("%-30s %d\n", "Número de Pontos Turísticos:", carta->pontos_turisticos);
+    printf("%-30s %.2f hab/km²\n","Densidade Populacional:", carta->densidade_populacional);
+    printf("%-30s %.2f reais\n","PIB per Capita: ", carta->pib_per_capta);
+}
 
-    printf("\n\nCarta 2:\n\n");
-    printf("Estado: %c\n", carta2->estado);
-    printf("Código: %s\n", carta2->cod_carta);
-    printf("Nome da Cidade: %s\n", carta2->nome);
-    printf("População: %u\n", carta2->populacao);
-    printf("Área: %.2f km²\n", carta2->area);
-    printf("PIB: %.2f bilhões de reais\n", carta2->PIB);
-    printf("Número de Pontos Turísticos: %d\n", carta2->pontos_turisticos);
-    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacao);
-    printf("PIB per Capita: %.2f reais\n", PIB_per_capta);
+void exibir_vencedor(const struct Carta *carta1, const struct Carta *carta2) {
+    printf("Comparação de Cartas:\n");
+
+    //População:
+    bool venceu = (carta1->populacao > carta2->populacao);
+
+    printf("População: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    //Área
+    venceu = (carta1->area > carta2->area);
+    printf("Área: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    //PIB
+    venceu = (carta1->PIB > carta2->PIB);
+    printf("PIB: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    //Pontos turísticos
+    venceu = (carta1->pontos_turisticos > carta2->pontos_turisticos);
+    printf("Pontos Turísticos: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    //Densidade Populacional
+    venceu = (carta1->densidade_populacional < carta2->densidade_populacional);
+    printf("Densidade Populacional: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    venceu = (carta1->pib_per_capta > carta2->pib_per_capta);
+    printf("PIB per Capita: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    venceu = (carta1->super_poder > carta2->super_poder);
+    printf("Super Poder: Carta %d venceu (%d)\n",(venceu ? 1 : 2), venceu);
+
+    printf(">>>>>> fim de jogo.");
 }
