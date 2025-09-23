a6b44dea0f33ba9829581c65b9a417349820b93e
Maicom Ferreira <maicomferre3242@gmail.com>
2025-09-23 10:03:42 -0300

    Super Trunfo Nível Aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,10 +3,11 @@
 
 struct Carta {
     char estado;
-    char cod_carta[4];
-    char nome[60];
-    float area;
-    float PIB;
+    char cod_carta[10];
+    unsigned int populacao;
+    char nome[60]; //considerando a cidade Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch(59)
+    double area;
+    double PIB;
     int pontos_turisticos;
 };
 
@@ -30,7 +31,8 @@ int main(void) {
     obter_carta(&carta1);
 
     sleep(1);
-    printf("\nPerfeito! Agora preencha a segunda carta.\n>----------------- | 2ª CARTA | >>>>\n\n");
+    printf("\nPerfeito! Agora preencha a segunda carta.\n");
+    printf(">----------------- | 2ª CARTA | >>>>\n\n");
     sleep(3);
 
     obter_carta(&carta2);
@@ -50,14 +52,18 @@ void obter_carta(struct Carta *c) {
     printf("\n\to codigo da carta: ");
     scanf(" %s", c->cod_carta);
 
-    printf("\no nome da cidade: ");
+    printf("\no nome da cidade(Ex: São Paulo): ");
     scanf(" %60[^\n]", c->nome);
+
+    printf("\na população da cidade: ");
+    scanf("%u", &c->populacao);
+
     printf("\na área da cidade(Ex: 17.5): ");
 
-    scanf(" %f", &c->area);
+    scanf(" %lf", &c->area);
     printf("\no PIB da cidade(Ex: 17.5): ");
 
-    scanf(" %f", &c->PIB);
+    scanf(" %lf", &c->PIB);
 
     printf("\nOs pontos turisticos(Ex: 12): ");
     scanf(" %i", &c->pontos_turisticos);
@@ -65,28 +71,33 @@ void obter_carta(struct Carta *c) {
 
 
 void exibir_cartas(struct Carta *carta1, struct Carta *carta2) {
-    printf("\n~~~~~~~~~~~~~~ ||  Super Trunfo  || ~~~~~~~~~~~~~~\n");
-    printf("~~~~~~~~~~~~~~ || CARTAS EM JOGO || ~~~~~~~~~~~~~~\n");
-    printf("                     CARTA 1 \n");
+    // Carta 1
+    double densidade_populacao = carta1->populacao / carta1->area;
+    double PIB_per_capta = (carta1->PIB * 1e9) / (double)carta1->populacao;
 
-    printf("Letra do Estado: %c\n",carta1->estado);
-    printf("Codigo da Carta: %s\n",carta1->cod_carta);
+    printf("\nCarta 1:\n\n");
+    printf("Estado: %c\n", carta1->estado);
+    printf("Código: %s\n", carta1->cod_carta);
     printf("Nome da Cidade: %s\n", carta1->nome);
-    printf("Área da Cidade: %f km²\n", carta1->area);
-    printf("PIB da Cidade: %f bilhões de reais\n", carta1->PIB);
-    printf("Pontos turísticos: %i\n", carta1->pontos_turisticos);
+    printf("População: %u\n", carta1->populacao);
+    printf("Área: %.2f km²\n", carta1->area);
+    printf("PIB: %.2f bilhões de reais\n", carta1->PIB);
+    printf("Número de Pontos Turísticos: %d\n", carta1->pontos_turisticos);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacao);
+    printf("PIB per Capita: %.2f reais\n", PIB_per_capta);
 
+    // Carta 2
+    densidade_populacao = carta2->populacao / (double)carta2->area;
+    PIB_per_capta = (carta2->PIB * 1e9) / (double)carta2->populacao;
 
-    printf("                     CARTA 2 \n");
-
-    printf("Letra do Estado: %c\n",carta2->estado);
-    printf("Codigo da Carta: %s\n",carta2->cod_carta);
+    printf("\n\nCarta 2:\n\n");
+    printf("Estado: %c\n", carta2->estado);
+    printf("Código: %s\n", carta2->cod_carta);
     printf("Nome da Cidade: %s\n", carta2->nome);
-    printf("Área da Cidade: %f  km²\n", carta2->area);
-    printf("PIB da Cidade: %f bilhões de reais\n", carta2->PIB);
-    printf("Pontos turísticos: %i\n", carta2->pontos_turisticos);
-
-    printf("~~~~~~~~~~~~~~ || CARTAS EM JOGO || ~~~~~~~~~~~~~~\n");
-    printf("~~~~~~~~~~~~~~ ||  Super Trunfo || ~~~~~~~~~~~~~~\n");
-
+    printf("População: %u\n", carta2->populacao);
+    printf("Área: %.2f km²\n", carta2->area);
+    printf("PIB: %.2f bilhões de reais\n", carta2->PIB);
+    printf("Número de Pontos Turísticos: %d\n", carta2->pontos_turisticos);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade_populacao);
+    printf("PIB per Capita: %.2f reais\n", PIB_per_capta);
 }
